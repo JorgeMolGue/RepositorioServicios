8079e3dd353863741eb7a0e8bb11d57009eb90ca
JorgeMolGue <146189148+JorgeMolGue@users.noreply.github.com>
2024-11-30 15:52:27 +0100

    Update variables.tf

--- ftp_ldap/variables.tf
+++ ftp_ldap/variables.tf
@@ -1,83 +1,100 @@
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
+variable "aws_session_token" {
+  type = string
+}
+
+variable "public_key" {
+  description = "Clave pública SSH para acceder a la instancia"
+  type        = string
+}
+
 variable "region" {
   description = "La región de AWS donde se crearán los recursos"
-  default     = ""
+  type        = string
 }
 
 variable "vpc1_cidr" {
   description = "CIDR para la VPC 1 (con subred pública y privada)"
-  default     = ""
+  type        = string
 }
 
 variable "vpc2_cidr" {
   description = "CIDR para la VPC 2 (con solo subred privada)"
-  default     = ""
+  type        = string
 }
 variable "public_subnet_cidr_vpc2" {
   description = "CIDR block for the public subnet in VPC 2"
-  default     = ""
+  type        = string
 }
 
 variable "public_subnet_vpc2_name" {
   description = "Name for the public subnet in VPC 2"
-  default     = ""
+  type        = string
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR para la subred pública"
-  default     = ""
+  type        = string
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR para la subred privada"
-  default     = ""
+  type        = string
 }
 
 variable "private_subnet_cidr_vpc2" {
   description = "CIDR para la subred privada de la VPC 2"
-  default     = ""
+  type        = string
 }
 
 variable "vpc1_name" {
   description = "Nombre de la VPC 1"
-  default     = ""
+  type        = string
 }
 
 variable "vpc2_name" {
   description = "Nombre de la VPC 2"
-  default     = ""
+  type        = string
 }
 
 variable "public_subnet_name" {
   description = "Nombre de la subred pública"
-  default     = ""
+  type        = string
 }
 
 variable "private_subnet_name" {
   description = "Nombre de la subred privada"
-  default     = ""
+  type        = string
 }
 
 variable "private_subnet_vpc2_name" {
   description = "Nombre de la subred privada de la VPC 2"
-  default     = ""
+  type        = string
 }
 ##instancia
 variable "instance_type" {
   description = "Tipo de la instancia EC2"
-  default     = ""
+  type        = string
 }
 
 variable "ami" {
   description = "ID de la AMI para la instancia"
-  default     = "" 
+  type        = string
 }
 
 variable "key_name" {
   description = "Nombre del par de claves para acceder a la instancia"
-  default     = "" 
+  type        = string
 }
 
 variable "bucket_name" {
   description = "Nombre del bucket S3"
-  default     = ""
+  type        = string
 }
